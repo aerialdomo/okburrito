6e8f07fe162b191bd7ddf67a8bd7f3ffbfa2acac
aerialdomo <christinaliu26@gmail.com>
2013-04-11 15:51:01 -0700

    did pseudo code for score calculation

---
+++ percent.py
@@ -0,0 +1,17 @@
+# remember that SQL Datatypes are INTEGERS!! NEED TO CHANGE TO DECIMAL!
+
+# Category: Spicy
+# "If I were a pokemon, I would be: " -pikachu -squirtle - charmander
+
+#psuedo code .1
+# opinion needs to get clicked via checkbox
+# when opinion picked, needs to relate to -1 to 1
+#user needs to choose a weight, which spans 0-1 
+# opinion * weight = score
+# score needs to go to a Category
+# Category needs to be displayed in bar graph
+
+
+answers = {'pikachu': 1,
+		   'squirtle': 1,
+		   'charmander': 1}
